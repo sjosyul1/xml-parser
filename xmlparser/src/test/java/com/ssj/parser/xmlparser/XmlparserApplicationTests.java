539d89e0431668e7b9380eac5c69eee5a5711daa
saisrinathjosyula <srinath.5893@gmail.com>
2024-03-30 20:49:53 -0500

    Initial commit

---
+++ xmlparser/src/test/java/com/ssj/parser/xmlparser/XmlparserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssj.parser.xmlparser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XmlparserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
